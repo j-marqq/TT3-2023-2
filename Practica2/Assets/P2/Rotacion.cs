7f1b837c95a3287b5727634042c8e3728a14310c
j-marqq <80246865+j-marqq@users.noreply.github.com>
2023-03-14 18:09:13 -0600

    Practica 2 añadida
    Se añadieron los archivos correspondientes en el folder con el mismo nombre. Se corrigió el nombre del directorio de la practica 1.

---
+++ Practica2/Assets/P2/Rotacion.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.0f, 0.2f, 0.0f)); 
+    }
+}
